44e596d21bcdf23e12984cfd5b84996ba371fa09
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2017-12-21 17:12:58 +0600

    **Registration completed**

--- SampleASP/Controllers/UserController.cs
+++ SampleASP/Controllers/UserController.cs
@@ -3,15 +3,126 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SampleASP.Models.DB;
+using System.Net;
+using System.Net.Mail;
 
 namespace SampleASP.Controllers
 {
     public class UserController : Controller
     {
-        // GET: User
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Registration()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration( [Bind(Exclude = "IsVerified, Token, IsLoggedin")] User user)
+        {
+            bool Status = false;
+            String Message = "";
+
+            if (ModelState.IsValid)
+            {
+                if (IsUserNameExist(user.Username))
+                {
+                    ModelState.AddModelError("UsernameExists", "You can not use this username, its already taken!");
+                }
+                else
+                {
+                    #region Generate Token
+                    user.Token = Guid.NewGuid();
+                    #endregion
+                     
+                    #region Passowrd Hashing
+                    user.Password = Crypto.Hash(user.Password);
+                    user.ConfirmPassword = Crypto.Hash(user.ConfirmPassword);
+                    #endregion
+
+                    user.IsVerified = false;
+                    user.IsLoggedin = false;
+
+                    #region Saving to Database
+                    using (SampleASPEntities SampleASPDatabase = new SampleASPEntities())
+                    {
+                        user.IsVerified = true;
+                        user.IsLoggedin = false;
+                        SampleASPDatabase.Users.Add(user);
+                        SampleASPDatabase.SaveChanges();
+
+                        #region Send Email Verification code
+                        SendEmailVerificationCode(user.Email, user.Token);
+                        Message = "Account created, verification code sent.";
+                        #endregion
+
+                        Status = true;
+                    }
+                    #endregion
+              
+                }
+            }
+            else
+            {
+                Message = "Invalid Message";
+            }
+
+            ViewBag.Status = Status;
+            ViewBag.Message = Message;
+
+            return View(user);
+        }
+
+        [NonAction]
+        private bool IsUserNameExist(string Username)
+        {
+            using (SampleASPEntities SampleASPDatabase = new SampleASPEntities())
+            {
+                var v = SampleASPDatabase.Users.Where(a => a.Username == Username).FirstOrDefault();
+
+                if (v != null)
+                    return true;
+            }
+
+            return false;
+        }
+
+        [NonAction]
+        private void SendEmailVerificationCode(string emailId, Guid token)
+        {
+
+            var verifyUrl = "User/verifyAccount/" + token;
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyUrl);
+
+            var fromEmail = new MailAddress("SampleASP1234@gmail.com", "SampleASP");
+            var toEmail = new MailAddress(emailId);
+            var fromEmailPassword = "RashedSourov1307045";
+
+            var subject = "Account verification";
+
+            var body = "<a hreaf = '" + link + "'>" + link + "</a>";
+
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+            };
+
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+            {
+                smtp.Send(message);
+            }
         }
     }
 }
---
+++ SampleASP/Models/DB/Extended/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleASP.Models.DB
+{
+    [MetadataType(typeof(UserMetadata))]
+    public partial class User
+    {
+        public string ConfirmPassword { get; set; }
+    }
+
+
+    public class UserMetadata
+    {
+        [Display(Name = "Username")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "User name is required!")]
+        public string Username { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required!")]
+        [MinLength(6, ErrorMessage = "Minimum 6 character required!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password does not match")]
+        public string ConfirmPassword { get; set; }
+
+
+        [Display(Name = "Email")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email id is not valid!")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+
+}
